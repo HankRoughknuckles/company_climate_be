fe183aec2ee9b7561a368d9d671e521fa793ed6a
Thomas Morris <tmorris@salsitasoft.com>
2020-05-30 05:07:18 +0200

    Make seed file for creating years between 1985 and the present year

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,6 @@
 # typed: strict
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+# create all the years from 1985 to present
+[*1985..Time.now.year].each do |year|
+  Year.find_or_create_by(name: year)
+end
