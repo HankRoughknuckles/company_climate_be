9dc07f6b993bae680bd16d07a6ff58b8a8afc157
Thomas Morris <tmorris@salsitasoft.com>
2020-06-20 18:32:50 +0200

    Change migrations so that defaults are nil for captured/produced
    nil represents that the company hasn't entered the information yet.
    Before when it was just 0.0, it was misleading because we couldn't be
    sure if their emissions were indeed 0 or if they just hadn't entered the
    data yet

--- db/migrate/20200530023856_add_total_co2_captured_to_company.rb
+++ db/migrate/20200530023856_add_total_co2_captured_to_company.rb
@@ -1,6 +1,6 @@
 # typed: true
 class AddTotalCo2CapturedToCompany < ActiveRecord::Migration[6.0]
   def change
-    add_column :companies, :total_co2_captured, :integer, default: 0
+    add_column :companies, :total_co2_captured, :float
   end
 end
